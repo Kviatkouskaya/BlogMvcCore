472c84942506e31c9b1f96ae45797e09c4b7b0ff
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-10-04 15:22:19 +0300

    DB created after first connection

--- BlogMvcCore/Models/UserDbContext.cs
+++ BlogMvcCore/Models/UserDbContext.cs
@@ -10,13 +10,5 @@ namespace BlogMvcCore.Models
                            : base(options) { Database.EnsureCreated(); }
         public DbSet<User> BlogUsers { get; set; }
         public DbSet<Post> Posts { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            User user = new(firstName: "Admin", secondName: "System", login: "admin", password: "12345678");
-            user.ID = 1;
-            builder.Entity<User>().HasData(user);
-        }
     }
 }
