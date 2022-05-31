dfdb9193da9b7359cde4f7ac0af0b79b933a238a
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-05-31 10:43:14 +0200

    added CommentWithLevel class and comments indent to post  page

--- InterfaceModel/Post.cs
+++ InterfaceModel/Post.cs
@@ -10,6 +10,6 @@ namespace BlogMvcCore.DomainModel
         public string Title { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
-        public List<Comment> Comments { get; set; }
+        public List<CommentWithLevel> Comments { get; set; }
     }
 }
