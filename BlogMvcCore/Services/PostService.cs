8f03ebf2a986e3535c8dd88154450687ab1e1546
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-06-23 11:49:54 +0200

    Comment refactoring

--- BlogMvcCore/Services/PostService.cs
+++ BlogMvcCore/Services/PostService.cs
@@ -21,10 +21,7 @@ namespace BlogMvcCore.Services
 
         public virtual void AddPost(string title, string postText, UserDomain user)
         {
-            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(postText))
-            {
-                return;
-            }
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(postText)) return;
 
             PostDomain newPost = new()
             {
