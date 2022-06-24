5d2e59552c4b1cdbf7989d1d331b91693f1fc9ca
Volha Kviatkouskaya <Volha_Kviatkouskaya@epam.com>
2022-06-24 12:56:47 +0200

    added GreetingTagHelper and refactored IndexView

---
+++ BlogMvcCore/TagHelpers/GreetingTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace BlogMvcCore.TagHelpers
+{
+    public class GreetingTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            var time = DateTime.Now.Hour;
+            var dayTime = time < 12 && time > 6 ? $"morning" :
+                              (time < 18 ? "afternoon" : "evening");
+            output.Content.SetContent($"Good {dayTime}!");
+        }
+    }
+}
